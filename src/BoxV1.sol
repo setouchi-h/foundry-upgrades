5bdb8d1ded91070a37c0817ba26a72c74393e91e
setouchi-h <setouchi.kazuki@gmail.com>
2023-08-31 18:58:30 +0900

    updated boxv1

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -2,9 +2,23 @@
 pragma solidity ^0.8.20;
 
 import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-contract BoxV1 is UUPSUpgradeable {
+// implementation contractにコンストラクタを設け例えばデプロイ時にnum=1とするとProxy(num=0)、Implementation(num=1)という状態になり、Proxyに反映されない
+// 代わりにinitialize()を設ける
+contract BoxV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 internal number;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize() public initializer {
+        __Ownable_init(); // sets the owner to meg.sender
+        __UUPSUpgradeable_init();
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
