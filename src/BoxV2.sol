b8da9d4388e0802998cd10bef242fdecf132220b
setouchi-h <setouchi.kazuki@gmail.com>
2023-08-31 22:52:36 +0900

    updated test

--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract BoxV2 {
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+contract BoxV2 is UUPSUpgradeable {
     uint256 internal number;
 
-    function setNumber(uint256 _number) external {}
+    function setNumber(uint256 newNumber) external {
+        number = newNumber;
+    }
 
     function getNumber() external view returns (uint256) {
         return number;
@@ -13,4 +17,6 @@ contract BoxV2 {
     function version() external pure returns (uint256) {
         return 2;
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
